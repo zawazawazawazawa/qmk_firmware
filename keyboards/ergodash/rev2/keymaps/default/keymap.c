f937b8b0f398f43c73013fc42eedf45e072f5c69
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2019-05-05 17:30:46 +0900

    remove extra keymap

--- keyboards/ergodash/rev2/keymaps/default/keymap.c
+++ keyboards/ergodash/rev2/keymaps/default/keymap.c
@@ -1,6 +1,5 @@
 #include QMK_KEYBOARD_H
 #include "process_unicode.h"
-#include "process_unicode_common.h"
 
 extern keymap_config_t keymap_config;
 
@@ -14,10 +13,6 @@ enum custom_keycodes {
   LOWER,
   RAISE,
   ADJUST,
-
-  // switch os
-  LNX,
-  OSX,
 };
 
 enum unicode_name {
@@ -48,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,                         KC_EQL, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        OSX,                            LNX, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______,                        _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,                        _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______, \
       KC_GRV, KC_LGUI, KC_LALT, EISU,           KC_LGUI,    KC_SPC ,_______,        X(THINKING_FACE), KC_ENT,KC_ESC,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
       ),
@@ -163,13 +158,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case LNX:
-      set_unicode_input_mode(UC_LNX);
-      break;
-    case OSX:
-      set_unicode_input_mode(UC_OSX);
-      break;
-
   }
   return true;
 }
