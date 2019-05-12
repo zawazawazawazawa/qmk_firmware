4c02c5e554ba9383f49b3aff69745ad5771a8400
mitubaEX <jun-nakamura@c-fo.com>
2019-05-13 01:26:17 +0900

    change keymap

--- keyboards/mituba_meishi/keymaps/default/keymap.c
+++ keyboards/mituba_meishi/keymaps/default/keymap.c
@@ -23,7 +23,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* Base */
-      KC_A, KC_B, KC_C, KC_D, KC_E, KC_F \
+      KC_MS_BTN1, KC_MS_UP, KC_MS_BTN2, KC_MS_RIGHT, KC_MS_DOWN, KC_MS_LEFT \
   ),
 };
 
