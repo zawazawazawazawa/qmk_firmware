5f352f91ac0b0d380598d4e5323e352858f6b3e8
mitubaEX <jun-nakamura@c-fo.com>
2019-06-27 23:54:32 +0900

    add lets_split keymap

--- keyboards/lets_split/keymaps/default/keymap.c
+++ keyboards/lets_split/keymaps/default/keymap.c
@@ -34,11 +34,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
-   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-   ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+   KC_T,  KC_R,   KC_E,    KC_W,    KC_Q,    KC_TAB,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+   KC_G,  KC_F,   KC_D,    KC_S,    KC_A,    KC_LCTL, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+   KC_B,  KC_V,   KC_C,    KC_X,    KC_Z,    KC_LSFT, KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  RAISE, \
+   KC_SPC,KC_LGUI,LOWER, KC_LALT, RAISE,   ADJUST,  KC_ENT,  KC_ESC,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
+
+// 左右逆にする
+// KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
+// KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+// KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
+// ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,
+
+
+  // KC_T,  KC_R,   KC_E,    KC_W,    KC_Q,    KC_TAB,
+  // KC_G,  KC_F,   KC_D,    KC_S,    KC_A,    KC_ESC,
+  // KC_B,  KC_V,   KC_C,    KC_X,    KC_Z,    KC_LSFT,
+  // KC_SPC,LOWER,  KC_LGUI, KC_LALT, KC_LCTL, ADJUST,
 
 /* Colemak
  * ,-----------------------------------------------------------------------------------.
@@ -87,12 +99,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_LOWER] = LAYOUT_ortho_4x12( \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+[_RAISE] = LAYOUT_ortho_4x12( \
+ KC_PERC, KC_DLR,  KC_HASH,  KC_AT,    KC_EXLM,  KC_TILD,   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
+ KC_F5,   KC_F4,   KC_F3,    KC_F2,    KC_F1,    KC_DEL,    KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+ KC_F11,  KC_F10,  KC_F9,    KC_F8,    KC_F7,    _______,   KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______, \
+ _______, _______, _______,  _______,  _______,  _______,   _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
+
+
+// KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,
+// KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
+// _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+// _______, _______, _______, _______, _______, _______,
+//
+//
+// KC_PERC, KC_DLR,  KC_HASH,  KC_AT,    KC_EXLM,  KC_TILD,
+// KC_F5,   KC_F4,   KC_F3,    KC_F2,    KC_F1,    KC_DEL,
+// KC_F11,  KC_F10,  KC_F9,    KC_F8,    KC_F7,    _______,
+// _______, _______, _______,  _______,  _______,  _______,
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
@@ -105,12 +129,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_RAISE] = LAYOUT_ortho_4x12( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+[_LOWER] = LAYOUT_ortho_4x12( \
+   KC_5,     KC_4,     KC_3,     KC_2,     KC_1,     KC_GRV,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+   KC_F5,    KC_F4,    KC_F3,    KC_F2,    KC_F1,    KC_DEL,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+   KC_F11,   KC_F10,   KC_F9,    KC_F8,    KC_F7,    _______,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
+   _______,  _______,  _______,  _______,  _______,  _______,  _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
+
+
+// KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+// KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
+// _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+// _______, _______, _______, _______, _______, _______,
+//
+//
+//   KC_5,     KC_4,     KC_3,     KC_2,     KC_1,     KC_GRV,
+//   KC_F5,    KC_F4,    KC_F3,    KC_F2,    KC_F1,    KC_DEL,
+//   KC_F11,   KC_F10,   KC_F9,    KC_F8,    KC_F7,    _______,
+//   _______,  _______,  _______,  _______,  _______,  _______,
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
