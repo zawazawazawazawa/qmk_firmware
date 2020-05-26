a7bc820febef2f31bea0252a141df8730dc0800d
shinichiro <bosse.de.poulet@gmail.com>
2020-05-26 20:22:47 +0900

    change keymap

--- keyboards/ergodash/rev2/keymaps/default/keymap.c
+++ keyboards/ergodash/rev2/keymaps/default/keymap.c
@@ -48,8 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,                         KC_EQL,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______,                        _______,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,                        _______,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______, \
-      KC_GRV,KC_LGUI,KC_LALT, X(THINKING_FACE), KC_LGUI,    KC_SPC ,_______,       THINKING_FACE_TYPE, KC_ENT,  KC_ESC,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,                        _______,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+      KC_GRV,KC_LGUI,KC_LALT, X(THINKING_FACE), KC_LGUI,    KC_SPC ,_______,       THINKING_FACE_TYPE, KC_ENT,  KC_RGUI,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
       ),
 
   /* Lower
